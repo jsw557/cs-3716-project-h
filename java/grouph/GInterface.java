878a7d141dd68b656b85ac427bb2a374c0280a81
jsw557 <jsw557@mun.ca>
2014-11-21 01:41:15 -0330

    almost functional GUI update

--- java/grouph/GInterface.java
+++ java/grouph/GInterface.java
@@ -1,17 +1,17 @@
-//package grouph;
+package grouph;
 
+import java.awt.event.*;
 import java.awt.*;
 import javax.swing.*;
 
 
-public class GInterface extends JFrame{
+public class GInterface extends JFrame  {
     
     public static void main(String[] args) {
 
         SwingUtilities.invokeLater(new Runnable() {
 
             public void run() {
-
                 GInterface window = new GInterface();
                 window.setVisible(true);
             }
@@ -22,23 +22,59 @@ public class GInterface extends JFrame{
     }
     public void initGUI() {
         
-        JPanel panel = new JPanel();
+        setLayout(new BorderLayout());
+        JPanel infoPanel = new JPanel();
+        JPanel groupPanel = new JPanel();
+        JPanel makePanel = new JPanel();
 
         JTextField groupSize = new JTextField();
-        JTextField classNum = new JTextField();
-        groupSize.setPreferredSize(new Dimension(150, 25));
-        classNum.setPreferredSize(new Dimension(150, 25));
+        groupSize.setPreferredSize(new Dimension(50, 25));
+        JTextArea classList = new JTextArea();
+        classList.setPreferredSize(new Dimension(150, 120));
+       // classList.setEditable(false);
+        JScrollPane classScrollPane = new JScrollPane(classList);
+        JTextArea groupList = new JTextArea();
+        groupList.setPreferredSize(new Dimension(150, 120));    
+        JScrollPane groupScrollPane = new JScrollPane(groupList);
+        groupList.setEditable(false);
+            
+
+
         JButton ok1 = new JButton("Set Size");
         JButton ok2 = new JButton("Select Class");
-	    
-        panel.add(groupSize); panel.add(ok1); 
-        panel.add(classNum); panel.add(ok2); 
+        JButton ok3 = new JButton("Make Groups");
+	   
+        infoPanel.add(groupSize);
+        infoPanel.add(ok1);
+        groupPanel.add(classList); 
+        groupPanel.add(ok2);
+        makePanel.add(groupList);
+        makePanel.add(ok3);       
         
-        add(panel);    
+        add(infoPanel, BorderLayout.NORTH);
+        add(groupPanel,BorderLayout.CENTER);
+        add(makePanel, BorderLayout.SOUTH);
+
         
+
+        ok1.addActionListener( new ActionListener () { public void actionPerformed(ActionEvent setGSize) {
+                controller.setGroupSize(groupSize.getVal());
+            //System.out.println("It worked!");
+            }
+        });
+        ok2.addActionListener( new ActionListener () {public void actionPerformed(ActionEvent getCrse) {
+                controller.parseFile(file);
+            }
+        });
+        ok3.addActionListener( new ActionListener () {public void actionPerformed(ActionEvent makeGrp) {
+                controller.makeGroup(stuList);
+            }
+        });
+        
+    
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setTitle("Group Maker");
-        setSize(300, 200);
+        setSize(400, 350);
         setLocationRelativeTo(null);
         
     }
