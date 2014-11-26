00b7066739bcc243f102a9d319c94be0f1eb1f1b
kks653 <kks653@mun.ca>
2014-11-26 00:34:13 -0330

    update parseFile() method & add some getter methods

--- java/grouph/Controller.java
+++ java/grouph/Controller.java
@@ -1,6 +1,5 @@
 package grouph;
 
-
 import java.awt.*;
 
 import javax.swing.*;
@@ -24,109 +23,110 @@ public class Controller {
 	private Student student;
     //private Match match;
 	//private Project project;
-	ArrayList<Student> StuList = new ArrayList<Student>(); // The list that holds students
-	int groupSize;
+	private ArrayList<Student> stuList; // The list that holds students
+	private int groupSize;
 	private String selectedFile;
-	String str=""; String gStr = "";
+	private String str = "";
+	String gStr = "";
 	private JFileChooser chooser = new JFileChooser();
     private final String NEWLINE =  "\n";
 	
    // public Controller()
     //{
     	//groupList = new ArrayList<Group>(0);
-    	//StuList= new ArrayList<Student>(0);
+    	//stuList= new ArrayList<Student>(0);
     //}
+    
 	// parse string from csv file
-	public void parseFile ( ) {
-		StuList = new ArrayList<Student>();
+	public void parseFile() {
+		stuList = new ArrayList<Student>();
 		String line = "";
 		final String DELIMITER = ",";
 		
 		
-			FileNameExtensionFilter filter = new 						FileNameExtensionFilter(
-            		"txt", "csv");
-           		chooser.setFileFilter(filter);
+		FileNameExtensionFilter filter = new FileNameExtensionFilter(
+            		"txt", "csv" );
+        chooser.setFileFilter( filter );
             
-           		int returnVal = chooser.showOpenDialog(null);
+        int returnVal = chooser.showOpenDialog( null );
                 
-            		if(returnVal == JFileChooser.APPROVE_OPTION) {
-            			System.out.println("You chose to open this file: " +
-                		chooser.getSelectedFile().getName());
-                        
-            		}
-            
-                
-			    try {
-	        	    BufferedReader br = new BufferedReader(
-	        		    new FileReader(chooser.getSelectedFile()));
-                
-	           
-	        	    while ((line = br.readLine()) != null) {
-	        	
-		        	// use comma as separator
-	        		    student = new Student();
-				        String[] info = new String[3];
-				        info = line.split(DELIMITER);
-	        		    student.name = info[0];
-	        		    student.id = info[1];
-	        		    student.gpa = Double.parseDouble(info[2]);
-	        	        str = str +" "+ (info[0] +" "+ info[1] + " " + info[2] + NEWLINE);
-	        		    StuList.add(student);
-	        	    }
-                
-                }
-                 catch (IOException ex) {
-	       		JOptionPane.showMessageDialog(null,
-	        		ex.getMessage());
-	        	    
-	   	        }
-		
-	  	}
+        if( returnVal == JFileChooser.APPROVE_OPTION ) {
+           	System.out.println( "You chose to open this file: " +
+           			chooser.getSelectedFile().getName() );                
+        }
+     
+		try {
+			BufferedReader br = new BufferedReader(
+					new FileReader( chooser.getSelectedFile() ) );
+         
+        	while( ( line = br.readLine() ) != null ) {
+        		// use comma as separator
+        	    student = new Student();
+			    String[] info = new String[3];
+			    info = line.split( DELIMITER );
+        		student.setName( info[0] );
+        		student.setId( info[1] );
+        		student.setGPA( Double.parseDouble( info[2] ) );
+
+        		stuList.add( student );        		
+        	}
+        }
+        catch( IOException ex ) {
+           	JOptionPane.showMessageDialog( null,
+           			ex.getMessage() );
+        }	
+	}
 	
 	// receives the size of group 
-	public void setGroupSize ( int size ) {
+	public void setGroupSize( int size ) {
 		groupSize = size;
 	}
 	
+	public int getGroupSize() {
+		return groupSize;
+	}
+	
 	// receives the Student list and make a group
-	public void makeGroup ( ArrayList<Student> StudentList,int size) {
-		System.out.println(StudentList.get(1).getName());
-		groupList = Match.makeGroups(StudentList,size);
-        for(Group grp : this.groupList){
+	public void makeGroup( ArrayList<Student> StudentList, int size ) {
+		// System.out.println(StudentList.get( 1 ).getName());
+		groupList = Match.makeGroups( StudentList, size );
+		
+        for( Group grp : this.groupList ) {
             gStr = gStr + grp.id +": " ;
-            for(Student s: grp.groupMems)
-            	{
+            
+            for( Student s: grp.groupMems ) {
             	gStr = gStr+s.getName()+", ";
-            	}
-            gStr= gStr.substring(0 , gStr.length()-2);
+            }
+            
+            gStr= gStr.substring( 0 , gStr.length()-2 );
             gStr = gStr+ NEWLINE;
         }
 	}
 	
-	public void makeGroupList ( Group group ) {
+	public void makeGroupList( Group group ) {
 		groupList = new ArrayList<Group>();
-		groupList.add(group);
+		groupList.add( group );
 	}
 	
-	/**
-	public void load( String file ) {
-		try {
-            FileNameExtensionFilter filter = new FileNameExtensionFilter(
-            		"JPG & GIF Images", "jpg", "gif");
-            chooser.setFileFilter(filter);
-            
-            int returnVal = chooser.showOpenDialog(parent);
-            
-            if(returnVal == JFileChooser.APPROVE_OPTION) {
-            	System.out.println("You chose to open this file: " +
-                chooser.getSelectedFile().getName());
-            }
-       	}
-       	catch( Exception ex ) {
-       		JOptionPane.showMessageDialog(null, ex.getMessage());
-        }
+	
+	public String getInfo() {
+		// reset String str
+		str = "";
+		
+		for(int i = 0; i < stuList.size(); i++) {
+			str = str + "Name: " + stuList.get( i ).getName() + ", ID: " +
+						stuList.get( i ).getId() + ", GPA: " + stuList.get( i ).getGPA() + NEWLINE;
+		}
+		
+		return str;
 	}
+	
+	public ArrayList<Student> getStuList() {
+		return stuList;
+	}
+}
 
+	/**
 	public void save( String file ) {
 		try {
             File fpw = new File( file );
@@ -146,5 +146,5 @@ public class Controller {
 	**/
 
 
-}
+
 
