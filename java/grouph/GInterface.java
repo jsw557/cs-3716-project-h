99836fc1d3ffe1f457a7118c2c4db53afcb19146
kks653 <kks653@mun.ca>
2014-11-26 00:36:25 -0330

    update to override the previos text area when load a new file other than appending

--- java/grouph/GInterface.java
+++ java/grouph/GInterface.java
@@ -14,13 +14,12 @@ public class GInterface extends JFrame  {
     	return controller;
     }
     
-    public static void main(String[] args) {
+    public static void main( String[] args ) {
 
-        SwingUtilities.invokeLater(new Runnable() {
-
+        SwingUtilities.invokeLater( new Runnable() {
             public void run() {
                 GInterface window = new GInterface();
-                window.setVisible(true);
+                window.setVisible( true );
                 
             }
         });
@@ -36,58 +35,70 @@ public class GInterface extends JFrame  {
         JPanel makePanel = new JPanel();
 
         final JTextField groupSize = new JTextField();
-        groupSize.setPreferredSize(new Dimension(200, 28));  // get size
+        groupSize.setPreferredSize(new Dimension( 200, 28 ) );  // get size
+        
         final JTextArea classList = new JTextArea();
-        classList.setPreferredSize(new Dimension(400, 320));   // select class
+        classList.setPreferredSize(new Dimension( 400, 320 ) );   // select class
        // classList.setEditable(false);
-        JScrollPane classScrollPane = new JScrollPane(classList);
+        JScrollPane classScrollPane = new JScrollPane( classList );
+        
         final JTextArea groupList = new JTextArea();
-        groupList.setPreferredSize(new Dimension(400, 320));    // make groups
-        JScrollPane groupScrollPane = new JScrollPane(groupList);
-        groupList.setEditable(false);
+        groupList.setPreferredSize( new Dimension( 400, 320 ) );    // make groups
+        JScrollPane groupScrollPane = new JScrollPane( groupList );
+        groupList.setEditable( false );
             
 
-
-        JButton ok1 = new JButton("Set Size");
-        JButton ok2 = new JButton("Select Class");
-        JButton ok3 = new JButton("Make Groups");
+        JButton ok1 = new JButton( "Set Size" );
+        JButton ok2 = new JButton( "Select Class" );
+        JButton ok3 = new JButton( "Make Groups" );
 	   
-        infoPanel.add(groupSize);
-        infoPanel.add(ok1);
-        groupPanel.add(classList); 
-        groupPanel.add(ok2);
-        makePanel.add(groupList);
-        makePanel.add(ok3);       
+        // add buttons in a frame
+        infoPanel.add( groupSize );
+        infoPanel.add( ok1 );
+        groupPanel.add( classList ); 
+        groupPanel.add( ok2 );
+        makePanel.add( groupList );
+        makePanel.add( ok3 );       
         
-        add(infoPanel, BorderLayout.NORTH);
-        add(groupPanel,BorderLayout.CENTER);
-        add(makePanel, BorderLayout.SOUTH);
+        // 
+        add( infoPanel, BorderLayout.NORTH );
+        add( groupPanel,BorderLayout.CENTER );
+        add( makePanel, BorderLayout.SOUTH );
 
         
-
-        ok1.addActionListener( new ActionListener () { public void actionPerformed(ActionEvent setGSize) {
-                controller.setGroupSize(Integer.parseInt(groupSize.getText()));
+        // Set Size Button
+        ok1.addActionListener( new ActionListener () { 
+        	public void actionPerformed( ActionEvent setGSize ) {
+                controller.setGroupSize( Integer.parseInt( groupSize.getText() ) );
            // System.out.println("It worked!"+controller.groupSize);
             }
         });
-        ok2.addActionListener( new ActionListener () {public void actionPerformed(ActionEvent getCrse) {
+        
+        // Select Class Button
+        ok2.addActionListener( new ActionListener () {
+        	public void actionPerformed( ActionEvent getCrse ) {
                 controller.parseFile();
-                classList.setText(controller.str);
+                // test printing
+                System.out.println( controller.getInfo() );
+                
+                classList.setText( controller.getInfo() );
             }
         });
-        ok3.addActionListener( new ActionListener () {public void actionPerformed(ActionEvent makeGrp) {
-        	System.out.println(controller.StuList.get(0).getName()+" "+controller.groupSize);
-                controller.makeGroup(controller.StuList,controller.groupSize);
-                groupList.setText(controller.gStr);
-                }
-            
+        
+        // Make Groups Button
+        ok3.addActionListener( new ActionListener () {
+        	public void actionPerformed( ActionEvent makeGrp ) {
+        		System.out.println( "Group Size: " + controller.groupSize );
+        		controller.makeGroup( controller.getStuList(), controller.getGroupSize() );
+        		groupList.setText( controller.gStr );
+        	}
         });
         
     
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setTitle("Group Maker");
-        setSize(600, 700);         // main frame
-        setLocationRelativeTo(null);
+        setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
+        setTitle( "Group Maker" );
+        setSize( 600, 700 );         // main frame
+        setLocationRelativeTo( null );
         
     }
 
